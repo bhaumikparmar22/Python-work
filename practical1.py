08ae6d2d91e93c74eb9339e9a6f5ace1d6199147
bhaumikparmar22 <bhaumikparmar22@gnu.ac.in>
2024-02-21 15:06:43 +0530

    21-2-2024

---
+++ practical1.py
@@ -0,0 +1,77 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================================================ RESTART: C:/Users/Administrator/Desktop/Python Work/practical1.py ================================================
+hon Progra
+
+================================================ RESTART: C:/Users/Administrator/Desktop/Python Work/practical1.py ================================================
+hon Progra
+Python Programin
+
+================================================ RESTART: C:/Users/Administrator/Desktop/Python Work/practical1.py ================================================
+hon Progra
+Python Programin
+thon Programing
+
+================================================ RESTART: C:/Users/Administrator/Desktop/Python Work/practical1.py ================================================
+hon Progra
+Python Programin
+thon Programing
+tnrri
+
+================================================ RESTART: C:/Users/Administrator/Desktop/Python Work/practical1.py ================================================
+hon Progra
+Python Programin
+thon Programing
+tnrri
+Pngn
+
+================================================ RESTART: C:/Users/Administrator/Desktop/Python Work/practical1.py ================================================
+hon Progra
+Python Programin
+thon Programing
+tnrri
+Pngn
+thon Program
+
+================================================ RESTART: C:/Users/Administrator/Desktop/Python Work/practical1.py ================================================
+hon Progra
+Python Programin
+thon Programing
+tnrri
+Pngn
+thon Program
+Python Programin
+>>> 
+================================================ RESTART: C:/Users/Administrator/Desktop/Python Work/practical1.py ================================================
+hon Progra
+Python Programin
+thon Programing
+tnrri
+Pngn
+thon Program
+Python Programin
+hon Programing
+>>> 
+================================================ RESTART: C:/Users/Administrator/Desktop/Python Work/practical1.py ================================================
+hon Progra
+Python Programin
+thon Programing
+tnrri
+Pngn
+thon Program
+Python Programin
+hon Programing
+yno
+>>> 
+================================================ RESTART: C:/Users/Administrator/Desktop/Python Work/practical1.py ================================================
+hon Progra
+Python Programin
+thon Programing
+tnrri
+Pngn
+thon Program
+Python Programin
+hon Programing
+yno
+gnimargorP nohtyP
